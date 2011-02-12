91823879f00e0c198345e1a37e8ac5c566e2e11a
Vlad Gh <vlad@vladgh.com>
2011-02-12 17:54:39 -0600

    Test

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-### Ubuntu LEMP Install Script
+### Ubuntu LEMP Install Script VGH
 
 ### Program Versions:
-NGINX_VER=0.9.4
-PHP_VER=5.3.5
-APC_VER=3.1.7
-SUHOSIN_VER=0.9.32.1
+NGINX_VER="0.9.4"
+PHP_VER="5.3.5"
+APC_VER="3.1.7"
+SUHOSIN_VER="0.9.32.1"
 
 CUR_DIR=$(pwd)
 

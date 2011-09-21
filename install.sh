9eb04974271d29e27068b0b052066218c27f6f58
Nimrod Mesika <nimrody@gmail.com>
2011-09-21 21:07:24 +0300

    Added imap support - tested on ubuntu 10.04.2 LTS

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ USER=$(who mom likes | awk '{print $1}')
 ESSENTIAL_PACKAGES="htop vim-nox binutils cpp flex gcc libarchive-zip-perl libc6-dev libcompress-zlib-perl m4 libpcre3 libpcre3-dev libssl-dev libpopt-dev lynx make perl perl-modules openssl unzip zip autoconf2.13 gnu-standards automake libtool bison build-essential zlib1g-dev ntp ntpdate autotools-dev g++ bc subversion psmisc"
 
 ### PHP Libraries
-PHP_LIBRARIES="install libmysqlclient-dev libcurl4-openssl-dev libgd2-xpm-dev libjpeg62-dev libpng3-dev libxpm-dev libfreetype6-dev libt1-dev libmcrypt-dev libxslt1-dev libbz2-dev libxml2-dev libevent-dev libltdl-dev libmagickwand-dev imagemagick libreadline-dev"
+PHP_LIBRARIES="install libmysqlclient-dev libcurl4-openssl-dev libgd2-xpm-dev libjpeg62-dev libpng3-dev libxpm-dev libfreetype6-dev libt1-dev libmcrypt-dev libxslt1-dev libbz2-dev libxml2-dev libevent-dev libltdl-dev libmagickwand-dev imagemagick libreadline-dev libc-client-dev"
 
 function progress() {
 # Simple progress indicator at the end of line (followed by "Done" when command is completed)
@@ -131,6 +131,9 @@ function install_php() {
 --with-readline \
 --with-fpm-user=www-data \
 --with-fpm-group=www-data \
+--with-imap \
+--with-imap-ssl \
+--with-kerberos \
 --disable-debug \
 --enable-fpm \
 --enable-cli \

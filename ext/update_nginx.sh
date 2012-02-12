c1aa51382b6549cfa194ff5e857005a3ebe5384a
Vlad <vgit@vladgh.com>
2012-02-11 22:23:37 -0600

    Fixed version check to support not just x.x.x, and fixed the indentation

--- ext/update_nginx.sh
+++ ext/update_nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 ###################################################################
-# Script to update Nginx to the latest version. 								  #
+# Script to update Nginx to the latest version.                   #
 # June 3rd 2011                                      Vlad Ghinea. #
 ###################################################################
 #
@@ -16,84 +16,84 @@ ARGS="$@"
 # Traps CTRL-C
 trap ctrl_c INT
 function ctrl_c() {
-	echo -e '\nCancelled by user'; if [ -n "$!" ]; then kill $!; fi; exit 1
+  echo -e '\nCancelled by user'; if [ -n "$!" ]; then kill $!; fi; exit 1
 }
 
 die() {
-	echo "ERROR: $1" > /dev/null 1>&2
-	exit 1
+  echo "ERROR: $1" > /dev/null 1>&2
+  exit 1
 }
 
 check_sanity() {
 
-	# Check if the script is run as root.
-	if [ $(/usr/bin/id -u) != "0" ]
-	then
-		die "Must be run by root user. Use 'sudo env PATH=\$PATH bash ...'"
-	fi
+  # Check if the script is run as root.
+  if [ $(/usr/bin/id -u) != "0" ]
+  then
+    die "Must be run by root user. Use 'sudo env PATH=\$PATH bash ...'"
+  fi
 
-	# A single argument allowed
-	[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+  # A single argument allowed
+  [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 
-	# Check if version is sane
-	echo $1 | grep -E -q '^[0-9].[0-9].[0-9]$' || die "Version number doesn't seem right; Please double check: $1"
+  # Check if version is sane
+  echo $1 | grep -E -q '^[0-9]+\.[0-9]+\.[0-9]+$' || die "Version number doesn't seem right; Please double check: $1"
 
-	NGINX_VER="$1"
-	DATE=`date +%Y.%m.%d`
-	SRCDIR=/tmp/nginx_$NGINX_VER-$DATE
-	NGINX_CMD=$(type -p nginx) # Get executable path
-	CONFIGURE_ARGS=$($NGINX_CMD -V 2>&1 | grep "configure arguments:" | cut -d " " -f3-) # Get original configure options
-	if [ ! -n "$CONFIGURE_ARGS" ]; then 	# tests to see if the argument is non empty
-		die "Previous arguments could not be loaded. You must run the command with 'sudo env PATH=\$PATH bash ...'"
-	fi
-	
-	# Check if version is the same
-	if [ $NGINX_VER == $($NGINX_CMD -v 2>&1 | cut -d "/" -f2) ]; then
-		die "This version number is already installed."
-	fi
+  NGINX_VER="$1"
+  DATE=`date +%Y.%m.%d`
+  SRCDIR=/tmp/nginx_$NGINX_VER-$DATE
+  NGINX_CMD=$(type -p nginx) # Get executable path
+  CONFIGURE_ARGS=$($NGINX_CMD -V 2>&1 | grep "configure arguments:" | cut -d " " -f3-) # Get original configure options
+  if [ ! -n "$CONFIGURE_ARGS" ]; then   # tests to see if the argument is non empty
+    die "Previous arguments could not be loaded. You must run the command with 'sudo env PATH=\$PATH bash ...'"
+  fi
+
+  # Check if version is the same
+  if [ $NGINX_VER == $($NGINX_CMD -v 2>&1 | cut -d "/" -f2) ]; then
+    die "This version number is already installed."
+  fi
 }
 
 get_nginx() {
 
-	# Download and extract source package
-	echo "Getting NginX"
-	mkdir $SRCDIR; cd $SRCDIR
-	wget http://nginx.org/download/nginx-$NGINX_VER.tar.gz
-	
-	if [ ! -f "nginx-$NGINX_VER.tar.gz" ]; then
-		die "This version could not be found on nginx.org/download."
-	fi	
-	
-	tar zxvf nginx-$NGINX_VER.tar.gz; cd nginx-$NGINX_VER
+  # Download and extract source package
+  echo "Getting NginX"
+  mkdir $SRCDIR; cd $SRCDIR
+  wget http://nginx.org/download/nginx-$NGINX_VER.tar.gz
+
+  if [ ! -f "nginx-$NGINX_VER.tar.gz" ]; then
+    die "This version could not be found on nginx.org/download."
+  fi
+
+  tar zxvf nginx-$NGINX_VER.tar.gz; cd nginx-$NGINX_VER
 
 }
 
 compile_nginx() {
 
-	# Configure and compile NginX with previous options
-	echo "Configure with previous options..."
-	./configure $CONFIGURE_ARGS
-	make
-	make install
+  # Configure and compile NginX with previous options
+  echo "Configure with previous options..."
+  ./configure $CONFIGURE_ARGS
+  make
+  make install
 
 }
 
 recover_conf() {
-	# Send the new default configuration to /tmp
-	[ -d /etc/nginx ] && mv /etc/nginx /tmp/nginx-$DATE
-	
-	# Recover previous configuration files
-	echo "Restore working Config..."
-	[ -d /etc/nginx.original  ] && mv /etc/nginx.original /etc/nginx
+  # Send the new default configuration to /tmp
+  [ -d /etc/nginx ] && mv /etc/nginx /tmp/nginx-$DATE
+
+  # Recover previous configuration files
+  echo "Restore working Config..."
+  [ -d /etc/nginx.original  ] && mv /etc/nginx.original /etc/nginx
 }
 
 restart_servers() {
-	echo "Restart NginX"
-	if [ $(ps -ef | grep -c "nginx") -gt 1 ]; then 
-		ps -e | grep "nginx" | awk '{print $1}' | xargs sudo kill -INT
-	fi
-	sleep 2
-	/etc/init.d/nginx start
+  echo "Restart NginX"
+  if [ $(ps -ef | grep -c "nginx") -gt 1 ]; then
+    ps -e | grep "nginx" | awk '{print $1}' | xargs sudo kill -INT
+  fi
+  sleep 2
+  /etc/init.d/nginx start
 }
 
 check_sanity $ARGS
@@ -108,3 +108,4 @@ restart_servers
 
 # Clean Sources
 rm -r $SRCDIR
+

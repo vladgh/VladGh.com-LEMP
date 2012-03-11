c19aa8a200ba92c5f6243ac7b5dc2c0eb421ee42
Vlad <vgit@vladgh.com>
2012-03-11 12:56:02 -0500

    Cleanup

--- install_files/mysql (copy).sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Installing MySQL server (this is escaped in order to be able to type the password in the initial dialog)
-function install_mysql() {
-  echo "Installing MySQL..." >&3
-  env DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server mysql-client & progress
-  mysql_secure_installation >&3
-}
-

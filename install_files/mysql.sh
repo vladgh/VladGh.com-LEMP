809e69b183276ea89fb6a4d0a9a1c22b7d73e9b2
Vlad <vgit@vladgh.com>
2012-05-08 16:09:20 -0500

    Moved mysql-client to php dependencies

--- install_files/mysql.sh
+++ install_files/mysql.sh
@@ -3,6 +3,6 @@
 # Installing MySQL server
 function install_mysql() {
   echo 'Installing MySQL...' >&3
-  env DEBIAN_FRONTEND=noninteractive apt-get -q -y install mysql-server mysql-client & progress
+  env DEBIAN_FRONTEND=noninteractive apt-get -q -y install mysql-server & progress
 }
 
